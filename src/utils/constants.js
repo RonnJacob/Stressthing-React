4cb96026639e3be33e971418cbc95189d15c8d6f
Ronn <jacob.r@husky.neu.edu>
2019-10-20 08:37:39 -0400

    Redeployment final attempt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const FETCH_URL = 'http://localhost:4200/api/';
-// export const FETCH_URL = 'https://cs5610-backend.herokuapp.com/api/';
+// export const FETCH_URL = 'https://stressthing-bkend.herokuapp.com/api/';
