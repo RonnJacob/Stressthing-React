fd72587bd144fe6320cbb178c11e578f323d8927
Ronn <jacob.r@husky.neu.edu>
2019-10-24 00:46:44 -0400

    Changed the Constants URL to new Heroku backend link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const FETCH_URL = 'http://localhost:4200/api/';
-// export const FETCH_URL = 'https://stressthing-bkend.herokuapp.com/api/';
-//
+// export const FETCH_URL = 'http://localhost:4200/api/';
+export const FETCH_URL = 'https://stressthing-backend.herokuapp.com/api/';
+
